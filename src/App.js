cf07a802fe7b3409d639da8197382e617cc8154c
Mohammadreza Khorasanchian <mkhorasanchian@gmail.com>
2022-04-25 21:45:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className=" bg-slate-500 h-full absolute w-full">
+      <div>
+        <h1>hi</h1>
+      </div>
+      <div>
+        <h2>
+          my name is mohammadreza
+        </h2>
+      </div>
+      <div>
+        <h3>
+          im trying to learn react and this is my website im gonna try to make it better over time.
+        </h3>
+      </div>
+      <h1 className=" text-green-400">hi gotvaran</h1>
+    </div>
+  );
+}
+
+export default App;
