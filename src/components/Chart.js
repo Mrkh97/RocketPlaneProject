6973879d32dc2a0f1a57078d3f44e00b7105372e
Mohammadreza Khorasanchian <mkhorasanchian@gmail.com>
2022-04-30 10:02:45 +0300

    modifying charts

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,25 +1,25 @@
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-  import { Line } from 'react-chartjs-2';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
-  
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const totalDuration = 5000;
 const delayBetweenPoints = totalDuration / 100;
 const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
@@ -67,11 +67,11 @@ export const options = {
 };
 
 let labels = [];
-for (let i=1;i<=100;i++){
+for (let i = 1; i <= 100; i++) {
   labels.push(i);
 }
 
-export const data = {
+export const taziTavsan = {
   labels,
   datasets: [
     {
@@ -82,15 +82,46 @@ export const data = {
     },
     {
       label: 'Rocket',
-      data: labels.map((item) => item*3),
+      data: labels.map((item) => item * 3),
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
   ],
 };
 
-export default function Chart(){
+export const taziHizZaman = {
+  labels,
+  datasets: [
+    {
+      label: 'Plane',
+      data: labels.map(() => 300),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+    
+  ],
+};
+
+export const tavsanHizZaman = {
+  labels,
+  datasets: [
+    
+    {
+      label: 'Rocket',
+      data: labels.map((item) => item * 3),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
+};
+
+export default function Chart() {
   return <div className=" p-10 bg-slate-300 m-2 rounded-lg">
-    <Line options={options} data={data} />
+    <Line options={options} data={taziTavsan} />
+    <div className=''>
+      <Line className=' ' options={options} data={taziHizZaman} />
+      <Line className='' options={options} data={tavsanHizZaman} />
+    </div>
+
   </div>
 }
